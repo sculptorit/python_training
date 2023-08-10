e6636eca954d6494be8498d088ada5825cf2b110
sculptorit <vladsmol@bk.ru>
2023-08-10 16:13:21 +0300

    Задание 15.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
+import pytest
+import random
+import string
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_digits(maxlen):
+    symbols = string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname=random_string('firstname', 10),
+                    lastname=random_string('lastname', 10),
+                    address=random_string('address', 10),
+                    email=random_string('email', 10),
+                    email2=random_string('email2', 10),
+                    email3=random_string('email3', 10),
+                    mobilephone=random_digits(10),
+                    homephone=random_digits(10),
+                    workphone=random_digits(10),
+                    secondaryphone=random_digits(10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="fghjk", lastname="fghnjmk",
-                      homephone="12323232121", mobilephone="79121232221", workphone="79531232221", secondaryphone="79324354221", email="test@mail.ru", email2="test2@mail.ru", email3="test3@mail.ru")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
