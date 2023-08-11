cd99f4f4c423d5bf6a5b8d6d1edeec93ac9629d1
sculptorit <vladsmol@bk.ru>
2023-08-11 23:32:59 +0300

    Дополнение к заданию 5

--- fixture/application.py
+++ fixture/application.py
@@ -2,35 +2,18 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
-
-
 class Application:
 
-    def __init__(self, browser, base_url):
-        if browser == "firefox":
-            self.wd = webdriver.Firefox()
-        elif browser == "chrome":
-            self.wd = webdriver.Chrome()
-        elif browser == "ie":
-            self.wd = webdriver.Ie()
-        else:
-            raise ValueError("WARNING: UNKNOWN BROWSER %s" % browser)
-        self.wd.implicitly_wait(5)
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-        self.base_url = base_url
-
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
 
     def open_home_page(self):
         wd = self.wd
-        wd.get(self.base_url)
+        wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
