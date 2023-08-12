a9056e11fda9f1b2a4fb925afc1afc23e90aa3f0
sculptorit <vladsmol@bk.ru>
2023-08-12 17:41:11 +0300

    Дополнение к заданиям 20-22

--- fixture/db.py
+++ fixture/db.py
@@ -28,49 +28,17 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address, email, email2, email3, "
-                           "homephone, mobilephone, workphone, seconphone from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2,"
+                           " email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname, address, email, email2, email3, homephone, mobilephone, workphone,
+                (id, firstname, lastname, address, homephone, mobilephone, workphone, email, email2, email3,
                  secondphone) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, home=homephone,
-                                    mobile=mobilephone, work=workphone, address=address, email=email,
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, homephone=homephone,
+                                    mobilephone=mobilephone, workphone=workphone, email=email,
                                     email2=email2, email3=email3, secondphone=secondphone))
         finally:
             cursor.close()
         return list
-
-    def get_contacts_not_in_group(self, group_id):
-        list = []
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("SELECT addressbook.id, addressbook.firstname, addressbook.lastname "
-                           "FROM  addressbook where addressbook.id not in ( select addressbook.id "
-                           "FROM  address_in_groups INNER JOIN addressbook "
-                           "ON addressbook.id = address_in_groups.id "
-                           "WHERE addressbook.deprecated = '0000-00-00 00:00:00' "
-                           "AND address_in_groups.deprecated = '0000-00-00 00:00:00' "
-                           "AND address_in_groups.group_id = '%s')" % group_id)
-
-            for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
-        finally:
-            cursor.close()
-        return list
-
-    def get_contacts_in_group_list(self, group):
-        list = []
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("select id, group_id from address_in_groups where group_id='%s'" % group)
-            for row in cursor:
-                (id, group_id) = row
-                list.append({'id': str(id), 'group_id': str(group_id)})
-        finally:
-            cursor.close()
-        return list
-
 
     def destroy(self):
         self.connection.close()
